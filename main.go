9c949f054badb4d812666baa4d531180f6322300
satyamkodale <satyamkodale@gmail.com>
2025-03-01 12:45:00 +0530

    connected with DB sucessfully

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
 	fmt.Println("Ready to talk with DB ")
+
+	// Define MongoDB connection URI
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(context.TODO())
+
+	// err already exists, used = to update its value instead of :=
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
 }
