e933f30f64324e6d68afed3eabed3576eca8874e
satyamkodale <satyamkodale@gmail.com>
2025-03-01 14:26:31 +0530

    performed operations with db

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,5 +31,33 @@ func main() {
 		log.Fatal("Could not connect to MongoDB:", err)
 	}
 	fmt.Println("Connected to MongoDB!")
+
+	// Select the database and collection
+	database := client.Database("talkwithDB")
+	collection := database.Collection("user")
+
+	// Insert a document
+	newUser1 := bson.D{{"name", "Satyam"}, {"age", 24}, {"email", "satyam@example.com"}}
+	insertResult1, err := collection.InsertOne(context.TODO(), newUser1)
+	newUser2 := bson.D{{"name", "Satyam"}, {"age", 25}, {"email", "satyam38@example.com"}}
+	insertResult2, err := collection.InsertOne(context.TODO(), newUser2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a document with ID:", insertResult1.InsertedID)
+	fmt.Println("Inserted a document with ID:", insertResult2.InsertedID)
+
+	// bson.M It represents a flexible JSON-like document where
+	// Find the document
+	// Decode is a function that converts the MongoDB document into a Go variable.
+	// bson.D (ordered key-value pairs) is used when order matters,
+	var result bson.M
+	err = collection.FindOne(context.TODO(), bson.D{{"name", "Satyam"}}).Decode(&result)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Found a document:", result)
 
 }
